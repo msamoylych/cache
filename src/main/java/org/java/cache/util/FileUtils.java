52f54145759ed60ca024464e2f8ae96f916d3339
Maxim Samoylych <msamoylych@sberbank-tele.com>
2017-07-28 16:54:04 +0300

    javadoc

--- src/main/java/org/java/cache/util/FileUtils.java
+++ src/main/java/org/java/cache/util/FileUtils.java
@@ -12,23 +12,21 @@ public final class FileUtils {
     private FileUtils() {
     }
 
+    /**
+     * Запись объекта в файл
+     * @param path путь к файлу
+     * @param value объект
+     */
     public static void writeFile(Path path, Serializable value) {
         // Сериализация
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        ObjectOutput out;
-        try {
-            out = new ObjectOutputStream(bos);
+        try (ObjectOutput out = new ObjectOutputStream(bos)) {
             out.writeObject(value);
             out.flush();
         } catch (IOException ex) {
             throw new IllegalStateException("Can't serialize value", ex);
-        } finally {
-            try {
-                bos.close();
-            } catch (IOException ex) {
-                // ignore close exception
-            }
         }
+
         byte[] bytes = bos.toByteArray();
 
         // Запись в файл
@@ -39,6 +37,11 @@ public final class FileUtils {
         }
     }
 
+    /**
+     * Чтение объекта из файла
+     * @param path путь к файлу
+     * @return объект
+     */
     public static Serializable readFile(Path path) {
         // Чтение файла
         byte[] bytes;
@@ -50,23 +53,17 @@ public final class FileUtils {
 
         // Десериализация
         ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
-        ObjectInput in = null;
-        try {
-            in = new ObjectInputStream(bis);
+        try (ObjectInput in = new ObjectInputStream(bis)) {
             return (Serializable) in.readObject();
         } catch (IOException | ClassCastException | ClassNotFoundException ex) {
             throw new IllegalStateException("Can't deserialize value", ex);
-        } finally {
-            try {
-                if (in != null) {
-                    in.close();
-                }
-            } catch (IOException ex) {
-                // ignore close exception
-            }
         }
     }
 
+    /**
+     * Удаление файла
+     * @param path путь к файлу
+     */
     public static void deleteFile(Path path) {
         try {
             Files.delete(path);
